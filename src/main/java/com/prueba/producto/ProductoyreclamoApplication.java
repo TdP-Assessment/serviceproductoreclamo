f188bd7b3fee32bf0c2d6d7168fdd52377f2f355
Your Name <you@example.com>
2021-04-23 12:29:18 -0500

    inicial

---
+++ src/main/java/com/prueba/producto/ProductoyreclamoApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.producto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductoyreclamoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductoyreclamoApplication.class, args);
+	}
+
+}
